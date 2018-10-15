43c74c73b16b52587ac109ad81fab8f21af06f75
Garrett Foster <garrett.d.foster@gmail.com>
2018-10-15 00:43:32 -0500

    working on 10, 20, 40, and 100 yd dash times
    conversion factors seem to be hard to figure out for the applyForce method

--- js/FieldScene.js
+++ js/FieldScene.js
@@ -1,7 +1,7 @@
 class FieldScene extends Phaser.Scene {
   constructor() {
     super(); //this is used to call the parent class Phase.Scene
-    this.in2px = 6; //6 inches per pixel at default zoom 
+    this.px_per_yd = 6; //6 pixels per yard at default zoon
   }
 
   //load assets
@@ -14,6 +14,7 @@ class FieldScene extends Phaser.Scene {
   create() {
     //setup boundaries in the matter.js world
     this.matter.world.setBounds(0, 0, game.config.width, game.config.height).disableGravity();
+    console.log(this.matter.world);
 
     //create background sprite
     let background = this.add.sprite(0, 0, 'field');
@@ -25,7 +26,7 @@ class FieldScene extends Phaser.Scene {
     background.setDisplaySize(720, 320);
 
     //create player from class player using text
-    this.player = new Player(this, 10 * this.in2px, game.config.height / 2, 'HB');
+    this.player = new Player(this, 10 * this.px_per_yd, game.config.height / 2, 'UB');
     console.log(this.player);
 
     //create camera to zoom in and follow player
@@ -39,5 +40,21 @@ class FieldScene extends Phaser.Scene {
   update() {
     // Allow the player to respond to key presses and move itself
     this.player.update();
+
+    if(this.player.sprite.x > 10 * this.px_per_yd && !this.t_start){
+      this.t_start = new Date();
+    } else if (this.player.sprite.x > 20 * this.px_per_yd && !this.t_10){
+      this.t_10 = (new Date() - this.t_start)/1000;
+      console.log('10 yard split: ' + this.t_10);
+    } else if(this.player.sprite.x > 30 * this.px_per_yd && !this.t_20){
+      this.t_20 = (new Date() - this.t_start)/1000;
+      console.log('20 yard split: ' + this.t_20);
+    } else if(this.player.sprite.x > 50 * this.px_per_yd && !this.t_40){
+      this.t_40 = (new Date() - this.t_start)/1000;
+      console.log('40 yard time: ' + this.t_40);
+    } else if(this.player.sprite.x > 110 * this.px_per_yd && !this.t_100){
+      this.t_100 = (new Date() - this.t_start)/1000;
+      console.log('100 yard time: ' + this.t_100);
+    }
   }
 }
--- js/Player.js
+++ js/Player.js
@@ -4,9 +4,9 @@ class Player {
 
         //create player at desired location with specified jersey number
         let text = scene.add.text(x, y, jersey_number, { font: '6px Arial', fill: '#ffffff' });
-        this.sprite = scene.matter.add.gameObject(text, { shape: { type: 'circle', radius: 6 } })
+        this.sprite = scene.matter.add.gameObject(text, { shape: { type: 'circle', radius: 6} })
             .setAngle(90)
-            .setMass(200); //90 kg which is roughly 200 lb
+            .setMass(207/32.2); //usain bolt weight in lb divided by gravity
 
         // Track the arrow keys & WASD
         const {LEFT, RIGHT, UP, DOWN} = Phaser.Input.Keyboard.KeyCodes;
@@ -21,28 +21,50 @@ class Player {
     update() {
         const keys = this.keys;
         const sprite = this.sprite;
-        const forceX = 0.270;
+        const forceX = 1/1500; // lbf * px/yd * s/step * s/step
         const forceY = forceX/2;
         let force_vector = {x: 0, y: 0};
 
         // Apply horizontal acceleration when left or right are applied
         if (keys.left.isDown) {
             force_vector.x = -forceX;
+            //sprite.setVelocityX(-10/60*this.scene.in2px); //concidence that in2px is 6 an px per meter is 6
         } else if (keys.right.isDown) {
             force_vector.x = forceX;
+            //sprite.setVelocityX(10/60*this.scene.in2px);
         } else {
             force_vector.x = 0;
+            //sprite.setVelocityX(0);
         }
 
         // Apply vertical acceleration when up or down are applied
         if (keys.down.isDown) {
             force_vector.y = forceY; //remember that y is down positive
+            //sprite.setVelocityY(6.5);
         } else if (keys.up.isDown) {
             force_vector.y = -forceY;
+            //sprite.setVelocityY(-6.5);
         } else {
             force_vector.y = 0;
+            //sprite.setVelocityY(0);
         }
 
         sprite.applyForce(force_vector);
+
+        //bound velocities
+        let max_speed = 10 * this.scene.px_per_yd / 60;
+        let vx = sprite.body.velocity.x;
+        if(vx > max_speed){
+            sprite.setVelocityX(max_speed);
+        } else if(vx < -1* max_speed){
+            sprite.setVelocityX(-1 * max_speed);
+        }
+
+        let vy = sprite.body.velocity.y;
+        if(vy > max_speed){
+            sprite.setVelocityY(max_speed);
+        } else if(vy < -1* max_speed){
+            sprite.setVelocityY(-1 * max_speed);
+        }
     }
 }
