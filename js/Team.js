79325c629b61dd7929568be6feac980ff1c8bb96
Rick Wight <obpwilliams482@gmail.edu>
2019-02-14 14:00:40 -0500

    Added constructor and internal variables for Team class

--- js/Team.js
+++ js/Team.js
@@ -1,3 +1,11 @@
 class Team {
+	constructor(name, jersey) {
+		this.name = name;
+		this.jersey = jersey;
 
+		// lists of 11 players for offense and defense. 
+		// should be eventually replaced with a single roster list and functions to pick the correct players for a givien lineup. 
+		this.offensiveLineup = new Array();
+		this.deffensiveLineup = new Array();
+	}
 }
