8299395ba05efbfcb007dc4461a260c9ebdbef06
Rick Wight <obpwilliams482@gmail.edu>
2019-02-19 14:04:30 -0500

    Add variables to Ball denoting if it is currently possessed, and by who. Ball can now "leave possession" of a player and be "caught" and possessed upon colliding with another player.

--- js/Ball.js
+++ js/Ball.js
@@ -2,6 +2,12 @@ class Ball {
 	constructor(scene, x, y) {
 		this.scene = scene;
 
+		// boolean variable, denotes if this ball is currently posessed by a player
+		this.posessed = false;
+		
+		// the player who currently posesses the ball. Currently null if none (should be a better way?)
+		this.posessedBy = null;
+
 		//create ball sprite at given location
 		var sprite = scene.physics.add.image(x, y, 'football').setDisplaySize(1.5 * 10 / 3 / 3 * this.scene.px_per_yd, 1.5 * 7 / 3 / 3 * this.scene.px_per_yd);
 
@@ -30,4 +36,23 @@ class Ball {
 	stop(){
 		this.body.stop();
 	}
+
+	// the ball is now posessed by the given player
+	caught(catcher) {
+		this.stop();
+		this.posessed = true;
+		this.posessedBy = catcher;
+		this.body.x = catcher.body.x - this.body.halfWidth;
+		this.body.y = catcher.body.y - this.body.halfHeight;
+	}
+
+	checkPosession() {
+		if (this.posessed) {
+			if (!this.scene.physics.world.overlap(this.posessedBy, this)) {
+				this.posessed = false;
+				this.posessedBy = null;
+				//alert("ball released!");
+			}
+		}
+	}// */
 }
