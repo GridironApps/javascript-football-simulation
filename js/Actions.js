e5bedc16260885bf2e964304fa64ca09562f5f13
Rick Wight <obpwilliams482@gmail.edu>
2019-02-19 19:01:30 -0500

    Added as-of-yet non-executable ThrowAction and StopAction classes

--- js/Actions.js
+++ js/Actions.js
@@ -1,4 +1,5 @@
 // single file for all player action classes
+// All Actions have an execute() function which takes a Player object and the FieldScene object
 
 class MoveAction {
 	constructor(coordinateString) {
@@ -11,7 +12,33 @@ class MoveAction {
 		this.ycoord = parseInt(coordinateString.substring(endFirstVar+1, endSecondVar));
 	}
 
-	execute(player) {
+	execute(player, scene) {
 		// move the player
+	}
+}
+
+class ThrowAction {
+	constructor(coordinateString) {
+		var endFirstVar = coordinateString.indexOf(",");
+		var endSecondVar = coordinateString.indexOf(",", endFirstVar+1);
+		if (endSecondVar <= -1) {
+			endSecondVar = coordinateString.length;
+		}
+		this.ballXcoord = parseInt(coordinateString.substring(0, endFirstVar));
+		this.ballYcoord = parseInt(coordinateString.substring(endFirstVar+1, endSecondVar));
+	}
+
+	execute(player, scene) {
+		// throw the ball
+	}
+}
+
+class StopAction {
+	constructor() {
+		// no values, do nothing
+	}
+
+	execute(player, scene) {
+
 	}
 }
