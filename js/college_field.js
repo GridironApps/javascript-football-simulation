5f3476e36622eee7d6242f0cb3fcc25a96df2337
Garrett Foster <garrett.d.foster@gmail.com>
2020-02-09 14:22:26 -0600

    making field data a js file for now to simplify loading and testing

---
+++ js/college_field.js
@@ -0,0 +1,20 @@
+var field = {
+    "type": "college",
+    "unit": "feet",
+    "total_length": 360,
+    "total_width": 160,
+    "border_thickness": 6,
+    "endzone_depth": 30,
+    "goalline_thickness": 0.667,
+    "hash_width": 40,
+    "line_thickness": 0.333,
+    "dash_length": 2,
+    "number_height": 6,
+    "number_width": 4,
+    "number_padding": 1,
+    "number_top": 27,
+    "goalpost_width": 18.5,
+    "crossbar_height": 10,
+    "goalpost_height": 30,
+    "goalpost_diameter": 0.333
+};
