5603404147f118d881654f00591eb6673ca4aca7
Garrett Foster <garrett.d.foster@gmail.com>
2020-02-09 00:08:16 -0600

    adding documentation

---
+++ js/presnap.js
@@ -0,0 +1,16 @@
+/**
+ * pre-snap phase module
+ * This phase of the game sim is responsible for getting the right players and setting up the initial coordinates of the offensive and defensive formations
+ *  
+ * inputs
+ * -field data
+ * -ball location (current yardline, assume centered for now) TODO allow ball to move between the hashes
+ * -offensive personnel and formation data
+ * -defensive personnel and formation data
+ * 
+ * outputs
+ * -offensive players with inital coordinates (left-to-right field)
+ * -defensive players wth initial coordinates
+ * -ball coordinates
+ * 
+ */
