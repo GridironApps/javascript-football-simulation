4dc65fc387878ada698261257eba1849fc41f90f
Garrett Foster <garrett.d.foster@gmail.com>
2020-08-03 02:57:04 -0500

    estimate where block occurs using 1D math

--- js/dnd.js
+++ js/dnd.js
@@ -28,102 +28,133 @@ if (bad_read.length > 0) {
 //===
 
 //initialize matchup list
-var gaps = ['E-', 'D-', 'C-', 'B-', 'A-', 'A+', 'B+', 'C+', 'D+', 'E+'];
+var gaps = ['E-', 'D-', 'C-', 'B-', 'A-', 'A+', 'B+', 'C+', 'D+', 'E+']; //FIXME, this might be better used when initializing the play
+var gap_locations = [-13, -7, -5, -3, -1, 1, 3, 5, 7, 13];
 var matchups = {};
 for (var i = 0; i < gaps.length; i++) {
     matchups[gaps[i]] = {
         'off_players': [],
         'block_dice': 0,
         'o_push_dice': 0,
-        'def_player': undefined,
+        'def_players': [],
         'shed_dice': 0,
-        'd_push_dice': 0
+        'd_push_dice': 0,
+        'gap_location': [gap_locations[i], 0],
+        'block_location': [gap_locations[i], 0]
     }
 };
 
-//loop through offense and update matchup variables
+//loop through offense and update matchup players and dice
 for (pos in offense) {
     if (offense[pos].job.hasOwnProperty('run_block')) {
         var gaps = offense[pos].job.run_block.gaps;
-        var push = offense[pos].attributes.strength / gaps.length;
-        var block = offense[pos].attributes.run_block / gaps.length;
+        var atts = offense[pos].attributes;
         for (var i = 0; i < gaps.length; i++) {
             matchups[gaps[i]].off_players.push(pos);
-            matchups[gaps[i]].block_dice += block;
-            matchups[gaps[i]].o_push_dice += push;
+            matchups[gaps[i]].block_dice += atts.run_block / gaps.length;
+            matchups[gaps[i]].o_push_dice += atts.strength / gaps.length;
         }
     }
 }
 
-//calculate location of each gaps collision
-
-
-//loop through defense and add shed rating to gap score
+//loop through defense and update matchup player(s)
 for (pos in defense) {
     if (defense[pos].job.gap != 'free') {
-        var gap = defense[pos].job.gap;
-        matchups[gap].def_player = pos;
-        matchups[gap].shed_dice += defense[pos].attributes.shed;
-        matchups[gap].d_push_dice += defense[pos].attributes.strength;
+        var gap = defense[pos].job.gap; //assuming each defender get's 1 gap
+        var atts = defense[pos].attributes;
+        matchups[gap].def_players.push(pos);
+        matchups[gap].shed_dice += atts.shed;
+        matchups[gap].d_push_dice += atts.strength;
     }
+}
+
+//loop through matchups and calculate location of initial block attempt
+for (gap in matchups) {
+    var m = matchups[gap]; //the current matchup
+
+    //figure out how long it will take defender to reach gap, FIXME assuming 1 defender per gap
+    //TODO update this to use L2 distance, using L1 for now to simplify (L2 is euclidian, L1 is taxi cab)
+    var defender = defense[m.def_players[0]];
+    var time_delay = defender.delay;
+    var dx_def = Math.abs(m.gap_location[0] - defender.location[0]);
+    var dy_def = Math.abs(m.gap_location[1] - defender.location[1]);
+    var speed_def = defender.attributes.speed;
+    var time_dx_def = dx_def * speed_def / 40;
+
+    //loop through potential blockers and figure out where they intersect the defender
+    var blockers = m.off_players;
+    for (var i = 0; i < blockers.length; i++) {
+        var blocker = offense[blockers[i]];
+        var dx_off = Math.abs(m.gap_location[0] - blocker.location[0]);
+        var dy_off = Math.abs(m.gap_location[1] - blocker.location[1]);
+        var time_dx_off = dx_off * blocker.attributes.speed / 40;
+
+        var y_total = dy_def + dy_off;
+        var dist_to_block = (40 / (speed_def + blocker.attributes.speed)) * (time_delay + time_dx_def - time_dx_off + y_total * speed_def / 40);
+        m.block_location[1] += (dist_to_block - dy_off);
+    }
+    //average the block location at the end
+    m.block_location[1] = m.block_location[1] / blockers.length;
 }
 
 //simulate matchups
 for (gap in matchups) {
-    
+
     var blocked = false;
-    var disadvantage = false;
+    var block_roll_mod = 'normal';
     var shed_block = false;
 
-    //see if the blocker(s) are able to initialize a block
-    if(matchups[gap].block_dice < 99 * Math.random()){
-        disadvantage = true;
+    //see if blocker arrived in time
+    if (matchups[gap].block_location[1] >= -0.5) {
 
-        //roll again, they are basically just in the way at this point
-        if(matchups[gap].block_dice >= 99 * Math.random()){
+        //see if the blocker(s) are able to initialize a block
+        if (matchups[gap].block_dice < roll(100)) {
+            block_roll_mod = 'disadvantaged';
+
+            //roll again, they are basically just in the way at this point
+            if (matchups[gap].block_dice >= roll(100)) {
+                blocked = true;
+            }
+        } else {
             blocked = true;
         }
-    }else{
-        blocked = true;
+    } else {
+        //check for a TFL
     }
 
-    if(blocked){
+    if (blocked) {
         //check to see if the defender is able to shed the block
-        var hold_block_score = matchups[gap].block_dice * Math.random();
-        if(disadvantage){
-            hold_block_score = Math.min(matchups[gap].block_dice * Math.random(), hold_block_score);
-        }
-        var shed_score = matchups[gap].shed_dice * Math.random();
+        var hold_block_score = roll(matchups[gap].block_dice, block_roll_mod);
+        var shed_score = roll(matchups[gap].shed_dice);
 
-        if(hold_block_score < shed_score){
+        if (hold_block_score < shed_score) {
             shed_block = true;
         }
     }
 
-    if(shed_block){
+    if (shed_block) {
         //defender has been delayed
         matchups[gap].push_score = 0; //TODO figure out a better representation
-    }else if(blocked){
+    } else if (blocked) {
         //figure out how far gap is pushed, can be negative if defender overpowers blocker(s)
-        var drive_block_score = matchups[gap].block_dice * Math.random();
-        if(disadvantage){
-            drive_block_score = Math.min(matchups[gap].block_dice * Math.random(), drive_block_score);
-        }
-        var o_push_score = matchups[gap].o_push_dice * Math.random() + drive_block_score;
-        var d_push_score = matchups[gap].d_push_dice * Math.random();
+        var drive_block_score = roll(matchups[gap].block_dice, block_roll_mod);
+        var o_push_score = roll(matchups[gap].o_push_dice) + drive_block_score;
+        var d_push_score = roll(matchups[gap].d_push_dice);
         matchups[gap].push_score = o_push_score - d_push_score;
-    }else{
+    } else {
         matchups[gap].push_score = -Infinity;
     }
+
+    
 }
 
 //printing the matchups to the screen
+var gaps = ['E-', 'D-', 'C-', 'B-', 'A-', 'A+', 'B+', 'C+', 'D+', 'E+']; //FIXME, this might be better used when initializing the play
 pbp('The matchups look like this:');
-var gaps = ['E-', 'D-', 'C-', 'B-', 'A-', 'A+', 'B+', 'C+', 'D+', 'E+'];
 for (var i = 0; i < gaps.length; i++) {
     var gap = gaps[i];
     var m = matchups[gap];
-    pbp('Gap ' + gap + ': ' + m.off_players + ' versus ' + m.def_player + ' resulting in a push of ' + m.push_score);
+    pbp('Gap ' + gap + ': ' + m.off_players + ' versus ' + m.def_players + ' met ' + Math.round(m.block_location[1]) + ' yards downfield and the pile was pushed with a score of ' + m.push_score);
 }
 
 //have the runner read the gaps
@@ -156,7 +187,6 @@ if (offense[pos].job.hasOwnProperty('run')) {
 } else {
     pbp('There seems to be a mixup in the backfield.');
 }
-
 
 /*
 //get location of default gap
@@ -206,4 +236,19 @@ function dist(a, b) {
     var dx = a[0] - b[0];
     var dy = a[1] - b[1];
     return Math.sqrt(dx * dx + dy * dy);
+}
+
+//roll function
+function roll(dice, modifier) {
+    var r = dice * Math.random();
+
+    if (modifier == 'advantage') {
+        r = Math.max(r, roll(dice));
+    }
+
+    if (modifier == 'disadvantage') {
+        r = Math.min(r, roll(dice));
+    }
+
+    return r;
 }
